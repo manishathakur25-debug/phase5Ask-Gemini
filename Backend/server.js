36137de2ba453a83abe89fa92f5626352e7c55cc
manishathakur25-debug <dollybisht24@navgurukul.org>
2026-09-24 15:30:33 +0530

    Fix Render port

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ import { GoogleGenAI } from "@google/genai"
 
 dotenv.config()
 const app=express()
-const PORT= process.env || 3000;
+const PORT= process.env.PORT || 3000;
 
 app.use(cors());
 // app.use(express.static("Frontend"))
